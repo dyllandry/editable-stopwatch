8b8e0cf15dd67fa6709911c6ce5c014329a41d68
dylan <dylanglandry@gmail.com>
2024-04-20 19:35:46 -0400

    add basic html elements

--- src/components/Stopwatch.vue
+++ src/components/Stopwatch.vue
@@ -2,8 +2,24 @@
 </script>
 
 <template>
-  <h1>Stopwatch</h1>
+    <form id="timeInput" class="time-input">
+        <input type="number" name="hours" max="23" min="0">
+        <span class="time-delimiter">:</span>
+        <input type="number" name="minutes" max="59" min="0">
+        <span class="time-delimiter">:</span>
+        <input type="number" name="seconds" max="59" min="0">
+    </form>
+    <button form="timeInput" type="submit">Start</button>
+    <button>Stop</button>
+    <button>Reset</button>
 </template>
 
 <style scoped>
+.time-input {
+    margin-bottom: 8px;
+}
+
+.time-delimiter {
+    margin: 0 8px;
+}
 </style>
