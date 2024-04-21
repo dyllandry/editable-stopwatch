596fc1a9eff457e6780c1645fc3db180ae393a30
dylan <dylanglandry@gmail.com>
2024-04-20 20:20:45 -0400

    handle input

--- src/components/Stopwatch.vue
+++ src/components/Stopwatch.vue
@@ -1,17 +1,73 @@
 <script setup lang="ts">
+import { ref, getCurrentInstance } from 'vue'
+
+const isRunning = ref<boolean>(false);
+
+const inputHours = ref<number | string>(0);
+const inputMinutes = ref<number | string>(0);
+const inputSeconds = ref<number | string>(0);
+
+const startDate = ref<Date | null>(null);
+const endDate = ref<Date | null>(null);
+
+const instance = getCurrentInstance();
+setInterval(() => instance.proxy.$forceUpdate(), 1000);
+
+function handleStart(event) {
+    event.preventDefault();
+    // Coerce type
+    if (typeof inputHours.value != 'number') {
+        inputHours.value = 0;
+    }
+    if (typeof inputMinutes.value != 'number') {
+        inputMinutes.value = 0;
+    }
+    if (typeof inputSeconds.value != 'number') {
+        inputSeconds.value = 0;
+    }
+
+    // Coerce size
+    inputHours.value = clamp(inputHours.value, 0, 23);
+    inputMinutes.value = clamp(inputMinutes.value, 0, 59);
+    inputSeconds.value = clamp(inputSeconds.value, 0, 59);
+
+    // TODO: set start date
+
+    isRunning.value = true;
+}
+
+function handleStop() {
+    isRunning.value = false;
+    // TODO: set end date
+}
+
+function handleReset() {
+    const confirmedReset = confirm("Are you sure you want to reset the stopwatch?");
+    if (!confirmedReset) {
+        return;
+    }
+    inputHours.value = 0;
+    inputMinutes.value = 0;
+    inputSeconds.value = 0;
+}
+
+function clamp(val, min, max) {
+    return Math.min(Math.max(val, min), max);
+}
 </script>
 
 <template>
-    <form id="timeInput" class="time-input">
-        <input type="number" name="hours" max="23" min="0">
+    <div class="time-input">
+        <input v-model="inputHours" type="number" name="hours">
         <span class="time-delimiter">:</span>
-        <input type="number" name="minutes" max="59" min="0">
+        <input v-model="inputMinutes" type="number" name="minutes">
         <span class="time-delimiter">:</span>
-        <input type="number" name="seconds" max="59" min="0">
-    </form>
-    <button form="timeInput" type="submit">Start</button>
-    <button>Stop</button>
-    <button>Reset</button>
+        <input v-model="inputSeconds" type="number" name="seconds">
+    </div>
+    
+    <button @click="handleStart" form="timeInput" type="submit">Start</button>
+    <button @click="handleStop">Stop</button>
+    <button @click="handleReset">Reset</button>
 </template>
 
 <style scoped>
