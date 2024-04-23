15e2623eb9221548390659d0dc8284113f1bf903
dylan <dylanglandry@gmail.com>
2024-04-22 23:24:22 -0400

    show duration when running

--- src/components/Stopwatch.vue
+++ src/components/Stopwatch.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, getCurrentInstance } from 'vue'
+import { ref, computed } from 'vue'
 
 const isRunning = ref<boolean>(false);
 
@@ -7,15 +7,19 @@ const inputHours = ref<number | string>(0);
 const inputMinutes = ref<number | string>(0);
 const inputSeconds = ref<number | string>(0);
 
-const startDate = ref<Date | null>(null);
-const endDate = ref<Date | null>(null);
+const startDate = ref<Date>(new Date());
+const endDate = ref<Date>(new Date());
 
-const instance = getCurrentInstance();
-setInterval(() => instance?.proxy?.$forceUpdate(), 1000);
+let now = ref<number>(Date.now());
+function updateNow() {
+    now.value = Date.now();
+}
+setInterval(updateNow, 1000);
 
 function onStart(event: MouseEvent) {
-    console.log(event);
     event.preventDefault();
+    updateNow();
+
     // Coerce type
     if (typeof inputHours.value != 'number') {
         inputHours.value = 0;
@@ -32,14 +36,20 @@ function onStart(event: MouseEvent) {
     inputMinutes.value = clamp(inputMinutes.value, 0, 59);
     inputSeconds.value = clamp(inputSeconds.value, 0, 59);
 
-    // TODO: set start date
+    const inputMs = (inputHours.value * 1000 * 60 * 60) + (inputMinutes.value * 1000 * 60) + (inputSeconds.value * 1000);
+
+    startDate.value = new Date(Date.now() - inputMs);
 
     isRunning.value = true;
 }
 
 function onStop() {
+    updateNow();
     isRunning.value = false;
-    // TODO: set end date
+    endDate.value = new Date();
+    inputHours.value = durationHours.value;
+    inputMinutes.value = durationMinutes.value;
+    inputSeconds.value = durationSeconds.value;
 }
 
 function onReset() {
@@ -50,7 +60,27 @@ function onReset() {
     inputHours.value = 0;
     inputMinutes.value = 0;
     inputSeconds.value = 0;
+    isRunning.value = false;
 }
+
+const duration = computed(() => {
+    if (isRunning) {
+        return now.value - startDate.value.getTime();
+    }
+    return endDate.value.getTime() - startDate.value.getTime();
+});
+
+const durationHours = computed(() => {
+    return clamp(Math.floor(duration.value / (1000 * 60 * 60)) % 24, 0, 23);
+});
+
+const durationMinutes = computed(() => {
+    return clamp(Math.floor(duration.value / (1000 * 60)) % 60, 0, 59);
+});
+
+const durationSeconds = computed(() => {
+    return clamp(Math.floor(duration.value / 1000) % 60, 0, 59);
+});
 
 function clamp(val: number, min: number, max: number) {
     return Math.min(Math.max(val, min), max);
@@ -58,11 +88,19 @@ function clamp(val: number, min: number, max: number) {
 </script>
 
 <template>
-    <div class="time-input">
+    <div v-if="isRunning" :style="{ marginBottom: '8px' }">
+        <span>{{ durationHours }}</span>
+        <span :style="{ margin: '0px 8px' }">:</span>
+        <span>{{ durationMinutes }}</span>
+        <span :style="{ margin: '0px 8px' }">:</span>
+        <span>{{ durationSeconds }}</span>
+    </div>
+
+    <div v-if="!isRunning" :style="{ marginBottom: '8px' }">
         <input v-model="inputHours" type="number" name="hours">
-        <span class="time-delimiter">:</span>
+        <span :style="{ margin: '0px 8px' }">:</span>
         <input v-model="inputMinutes" type="number" name="minutes">
-        <span class="time-delimiter">:</span>
+        <span :style="{ margin: '0px 8px' }">:</span>
         <input v-model="inputSeconds" type="number" name="seconds">
     </div>
     
@@ -72,11 +110,5 @@ function clamp(val: number, min: number, max: number) {
 </template>
 
 <style scoped>
-.time-input {
-    margin-bottom: 8px;
-}
 
-.time-delimiter {
-    margin: 0 8px;
-}
 </style>
